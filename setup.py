71209f5a2b586be795cdbfb5c4dac5d082341f96
Remy Muhire <rmuhire@exuus.com>
2016-11-21 14:13:21 +0200

    app settings

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='apicartix',
+      version='0.1',
+      description='Rwanda Administrative Location',
+      url='https://github.com/rmuhire/myrw',
+      author='exuus',
+      author_email='devteam@exuus.com',
+      license='exuus',
+      packages=['apicartix'],
+      install_requires = [
+
+      ],
+      include_package_data=True,
+      zip_safe=False)
