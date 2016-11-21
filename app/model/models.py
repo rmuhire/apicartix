662e54f91e6cf49fbaca9c8e1641ee5268e7c08f
Remy Muhire <rmuhire@exuus.com>
2016-11-21 16:05:44 +0200

    Funding Partner

--- app/model/models.py
+++ app/model/models.py
@@ -11,12 +11,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
 class SavingGroup(db.Model):
+    __tablename__ = 'saving_group'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     year = db.Column(db.Integer)
     member_female = db.Column(db.Integer)
     member_male = db.Column(db.Integer)
     sector_id = db.Column(db.Integer)
+
+    Amount = db.relationship('Amount', backref='saving_group', lazy='dynamic')
 
     def __init__(self, name, year, member_female, member_male, sector_id):
         self.name = name,
@@ -26,10 +29,44 @@ class SavingGroup(db.Model):
         self.sector_id = sector_id
 
 class Amount(db.Model):
-    id  = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     saving = db.Column(db.Float)
     borrowing = db.Column(db.Float)
     year = db.Column(db.Integer)
-    sg_id =  db.Column(db.Integer)
+    sg_id =  db.Column(db.Integer, db.ForeignKey('saving_group.id'))
+
+    def __init__(self, saving, borrowing, year, sg_id):
+        self.saving = saving
+        self.borrowing = borrowing
+        self.year = year
+        self.sg_id = sg_id
+
+
+class Funding(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), unique = True)
+    email = db.Column(db.String(60))
+    telephone = db.Column(db.String(30))
+    website = db.Column(db.String(60))
+    picture = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+    cp_name = db.Column(db.String(60))
+    cp_email = db.Column(db.String(60), unique = True)
+    cp_telephone = db.Column(db.String(30), unique = True)
+
+    def __init__(self, name, email, telephone, website, picture, address, cp_name, cp_email, cp_telephone):
+        self.name = name
+        self.email = email
+        self.telephone = telephone
+        self.website = website
+        self.picture = picture
+        self.address = address
+        self.cp_name = cp_name
+        self.cp_email = cp_email
+        self.cp_telephone = cp_telephone
+
+
+
+
 
     
