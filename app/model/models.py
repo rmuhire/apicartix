be66e97c6cb7c333c40729887b9b275d87256141
Remy Muhire <rmuhire@exuus.com>
2016-11-21 15:43:04 +0200

    amount class

--- app/model/models.py
+++ app/model/models.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
 class SavingGroup(db.Model):
-    sg_id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     year = db.Column(db.Integer)
     member_female = db.Column(db.Integer)
@@ -19,6 +19,17 @@ class SavingGroup(db.Model):
     sector_id = db.Column(db.Integer)
 
     def __init__(self, name, year, member_female, member_male, sector_id):
-        
+        self.name = name,
+        self.year = year
+        self.member_female = member_female
+        self.member_male = member_male
+        self.sector_id = sector_id
+
+class Amount(db.Model):
+    id  = db.Column(db.Integer, primary_key = True)
+    saving = db.Column(db.Float)
+    borrowing = db.Column(db.Float)
+    year = db.Column(db.Integer)
+    sg_id =  db.Column(db.Integer)
 
     
