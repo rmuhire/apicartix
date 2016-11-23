cc01cadeabae809381262c1165a430631ca63c4a
hirwa dieume <dhirwa@exuus.com>
2016-11-23 15:26:47 +0200

    Excel to JSON algo

--- excelup.py
+++ excelup.py
@@ -8,6 +8,12 @@ for sheet in wb.sheets():
     number_of_rows = sheet.nrows
     number_of_columns = sheet.ncols
 
+    for r in range(0,1):
+        vs=[]
+        for c in range(number_of_columns):
+            v = (sheet.cell(r,c).value)
+            vs.append(v)
+
     items = []
 
     rows = []
@@ -26,11 +32,13 @@ for sheet in wb.sheets():
         item = values
         items.append(item)
 
-
-
 data=[]
+hds=[]
 
 for item in items:
     data.append(item)
     datas = json.dumps(data,sort_keys=True, indent=4)
 print datas
+for vd in vs:
+    hds.append(vd)
+print hds
