e0c228f3115bcf782a0c833a1a87f86b74d0e934
hirwa dieume <dhirwa@exuus.com>
2016-11-23 12:09:10 +0200

    get

--- app/api/get.py
+++ app/api/get.py
@@ -24,6 +24,69 @@ def get_svgname(name):
     svg = SavingGroup.query.filter_by(name=name).first()
     if svg :
         result=sg_schema.dump(svg)
-        return jsonify({'Message':'1','Saving Group':result.data})
+        return jsonify({'Saving Group':result.data})
+    else:
+        return jsonify({'Message':'0'})
+
+
+@app.route('/getAmount')
+def get_am():
+    ams = Amount.query.all()
+    result = amounts_schema.dump(ams).data
+    return jsonify({'Amounts':result})
+
+@app.route('/getAmount/<int:sgid>')
+def get_ams(sgid):
+    ams = Amount.query.filter_by(sg_id=sgid).first()
+    if ams:
+        result = amount_schema.dump(ams)
+        return jsonify({'SG-Amount':result.data})
+    else:
+        return jsonify({'Message':'0'})
+
+@app.route('/getfundings')
+def get_fu():
+    funs = Funding.query.all()
+    result = fundings_schema.dump(funs).data
+    return jsonify({'Funding NGOs':result})
+
+@app.route('/getfunding/<name>')
+def get_fun(name):
+    fun = Funding.query.filter_by(name=name)
+    if fun:
+        result = funding_schema.dump(fun)
+        return jsonify({'Funding NGO':result.data})
+
+    else:
+        return jsonify({'Message':'0'})
+
+@app.route('/getpartners')
+def get_pa():
+    parts = Partner.query.all()
+    result = partners_schema.dump(parts).data
+    return jsonify({'Partner NGOs':result})
+
+@app.route('/getpartner/<name>')
+def get_pas(name):
+    fun = Funding.query.filter_by(name=name)
+    if fun:
+        result = funding_schema.dump(fun)
+        return jsonify({'Partner NGO':result.data})
+
+    else:
+        return jsonify({'Message':'0'})
+
+@app.route('/getFuPas')
+def get_fupas():
+    fupa = Sgs.query.all()
+    result = sgfps_schema.dump(fupa)
+    return jsonify({'Funding-Partner':result.data})
+
+@app.route('/getFuPa/<int:id>')
+def getfup(id):
+    fupa = Sgs.query.filter_by(id=id)
+    if fupa:
+        result = sgfp_schema.dump(fupa)
+        return jsonify({'Funding-Partner':result.data})
     else:
         return jsonify({'Message':'0'})
