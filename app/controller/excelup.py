01648eeea6fbe91c44fe488782ad23811a7ea353
hirwa dieume <dhirwa@exuus.com>
2016-11-25 17:46:52 +0200

    Commit

---
+++ app/controller/excelup.py
@@ -0,0 +1,37 @@
+from xlrd import open_workbook
+import json
+
+
+
+class Excellento:
+    def __init__(self, filepath):
+        self.filepath =filepath
+
+    def json(self):
+        wb = open_workbook(self.filepath)
+        for sheet in wb.sheets():
+            number_of_rows = sheet.nrows
+            number_of_columns = sheet.ncols
+
+            for row in range(0, 1):
+                header_sheet = []
+                for col in range(number_of_columns):
+                    header_cell = (sheet.cell(row, col).value)
+                    header_sheet.append(header_cell)
+
+            items = []
+
+            for row in range(1, number_of_rows):
+                data_json = {}
+                for col in range(number_of_columns):
+                    value = (sheet.cell(row, col).value)
+                    try:
+                        value = str(int(value))
+
+                    except ValueError:
+                        pass
+                    finally:
+                        data_json[header_sheet[col]] = value
+
+                items.append(data_json)
+        return json.dumps(items,sort_keys=True, indent=4)
