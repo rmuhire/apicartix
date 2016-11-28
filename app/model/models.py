53160e07c0623eaea2c45b141a39098a23fcde9b
Remy Muhire <rmuhire@exuus.com>
2016-11-28 11:07:28 +0200

    models

--- app/model/models.py
+++ app/model/models.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://muhireremy:8@localhost/afr_cartix'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
@@ -49,62 +48,44 @@ class Amount(db.Model):
         self.sg_id = sg_id
 
 
-class Funding(db.Model):
+class Ngo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), unique = True)
     email = db.Column(db.String(60))
     telephone = db.Column(db.String(30))
     website = db.Column(db.String(60))
+    category = db.Column(db.String(40))
     picture = db.Column(db.String(100))
     address = db.Column(db.String(200))
     cp_name = db.Column(db.String(60))
     cp_email = db.Column(db.String(60), unique = True)
     cp_telephone = db.Column(db.String(30), unique = True)
+    password = db.Column(db.String(40))
 
-    sgs = db.relationship('Sgs', backref='funding', lazy='dynamic')
+    sgs = db.relationship('Sgs', backref='ngo', lazy='dynamic')
 
-    def __init__(self, name, email, telephone, website, picture, address, cp_name, cp_email, cp_telephone):
+    def __init__(self, name, email, telephone, website, category, picture, address, cp_name, cp_email, cp_telephone, password):
         self.name = name
         self.email = email
         self.telephone = telephone
         self.website = website
+        self.category = category
         self.picture = picture
         self.address = address
         self.cp_name = cp_name
         self.cp_email = cp_email
         self.cp_telephone = cp_telephone
+        self.password = password
 
 
-class Partner(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    email = db.Column(db.String(60))
-    telephone = db.Column(db.String(30))
-    website = db.Column(db.String(60))
-    picture = db.Column(db.String(100))
-    address = db.Column(db.String(200))
-    cp_name = db.Column(db.String(60))
-    cp_email = db.Column(db.String(60), unique=True)
-    cp_telephone = db.Column(db.String(30), unique=True)
 
-    sgs = db.relationship('Sgs', backref='partner', lazy='dynamic')
 
-    def __init__(self, name, email, telephone, website, picture, address, cp_name, cp_email, cp_telephone):
-        self.name = name
-        self.email = email
-        self.telephone = telephone
-        self.website = website
-        self.picture = picture
-        self.address = address
-        self.cp_name = cp_name
-        self.cp_email = cp_email
-        self.cp_telephone = cp_telephone
 
 
 class Sgs(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    partner_id = db.Column(db.Integer, db.ForeignKey('partner.id'))
-    funding_id =  db.Column(db.Integer, db.ForeignKey('funding.id'))
+    partner_id = db.Column(db.Integer, db.ForeignKey('ngo.id'))
+    funding_id = db.Column(db.Integer, db.ForeignKey('ngo.id'))
 
     def __init__(self, partner_id, funding_id):
         self.partner_id = partner_id
