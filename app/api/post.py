e6a98750d87eb88035552c0dbbb751c112b7b383
Remy Muhire <rmuhire@exuus.com>
2016-11-28 16:43:18 +0200

    visualize excel

--- app/api/post.py
+++ app/api/post.py
@@ -2,13 +2,17 @@ from app.model.models import *
 from app.model.schema import *
 from flask import jsonify,request
 from app.controller.exellentodb import Excellentodb
+from app.controller.exellentodb import Excellento
 
 
 @app.route('/api/v1/exellento',methods=['POST'])
 def excellento():
     data = Excellentodb('all.xlsx').todb()
-
-
     return jsonify({'data':data})
 
+
+@app.route('/api/v1/visualize', methods=['POST'])
+def visualize():
+    data = Excellento('all.xlsx').json()
+    return jsonify({'data':data})
 
