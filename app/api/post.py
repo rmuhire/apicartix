f4213a4f418babb7e7eee90a39eebb6157395430
hirwa dieume <dhirwa@exuus.com>
2016-11-28 17:02:47 +0200

    Naming post APIs

--- app/api/post.py
+++ app/api/post.py
@@ -2,80 +2,10 @@ from app.model.models import *
 from app.model.schema import *
 from flask import jsonify,request
 import datetime
-from app.controllers.Excellento import exc_json
-
-
-@app.route('/submission/'.methods=['POST'])
-def subm():
-    json_data=Excellento('path')
-    if not json_data:
-        return jsonify({'Message':'No data provided'})
-    data,errors=sg_schema.load(json_data)
-    dat,errors=amount_schema.load(json_data)
-    if errors:
-        return jsonify(errors), 422
-
-    try:
-        svgs=SavingGroup(
-            name=data['name'],
-            year=data['year'],
-            member_female=data['member_female'],
-            member_male=data['member_male'],
-            sector_id=data['sector_id'],
-            regDate=datetime.datetime.utcnow()
-            )
-        amt = Amount(
-            saving = dat['saving'],
-            borrowing = dat['borrowing'],
-            year = dat['year'],
-            sg_id = dat['sg_id']
-            )
-        sg = Sgs(
-
-        )
-
-        db.session.add(amt)
-        db.session.add(svgs)
-        db.session.commit()
-
-        res = amount_schema.dump(Amount.query.get(amt.id))
-        result=sg_schema.dump(SavingGroup.query.get(svgs.id))
-        return jsonify({'Saving group':result.data,'Amount':res.data})
-
-    except:
-        return jsonify({'Message':'0'})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #==================================================INDIVIDUAL POST==========================================================
 
-@app.route('/savinggroup/',methods=['POST'])
+@app.route('/api/v1/savinggroup/',methods=['POST'])
 def svg():
     json_data=request.get_json()
     if not json_data:
@@ -101,7 +31,7 @@ def svg():
     except:
         return jsonify({'Message':'0'})
 
-@app.route('/amount/',methods=['POST'])
+@app.route('/api/v1/amount/',methods=['POST'])
 def amount():
     json_data=request.get_json()
     if not json_data:
@@ -125,59 +55,38 @@ def amount():
     except:
         return jsonify({'Message':'0'})
 
-@app.route('/funding/',methods=['POST'])
+@app.route('/api/v1/ngo/',methods=['POST'])
 def fundg():
     json_data=request.get_json()
     if not json_data:
         return jsonify({'Message':'No data provided'})
-    data,errors=funding_schema.load(json_data)
+    data,errors=ngo_schema.load(json_data)
     if errors:
         return jsonify(errors), 422
 
-    try:
-        fund = Funding(
-            name=data['name'],
-            email=data['email'],
-            username=data['username'],
-            cp_name=data['cp_name'],
-            cp_email=data['cp_email'],
-            cp_telephone=data['cp_telephone'],
-            password=data['password']
-            )
-        db.session.add(fund)
-        db.session.commit()
-        result = funding_schema.dump(Funding.query.get(fund.id))
-        return jsonify({'Funding':result.data})
-    except:
-        return jsonify({'Message':'0'})
-
-@app.route('/partner/',methods=['POST'])
-def partn():
-    json_data=request.get_json()
-    if not json_data:
-        return jsonify({'Message':'No data provided'})
-    data,errors=partner_schema.load(json_data)
-    if errors:
-        return jsonify(errors), 422
-
-    try:
-        fund = Partner(
-            name = data['name'],
-            email = data['email'],
+    #try:
+    ngo = Ngo(
+            name= data['name'],
+            email= data['email'],
             username = data['username'],
+            category = data['category'],
+            website = "",
+            picture= "",
+            telephone = "",
+            address = data['address'],
             cp_name = data['cp_name'],
             cp_email = data['cp_email'],
             cp_telephone = data['cp_telephone'],
-            password = data['password']
+            password = data['password'],
             )
-        db.session.add(fund)
-        db.session.commit()
-        result = partner_schema.dump(Partner.query.get(fund.id))
-        return jsonify({'Partner':result.data})
-    except:
-        return jsonify({'Message':'0'})
+    db.session.add(ngo)
+    db.session.commit()
+    result = ngo_schema.dump(Ngo.query.get(ngo.id))
+    return jsonify({'NGO':result.data})
+    #except:
+    #    return jsonify({'Message':'0'})
 
-@app.route('/pafu/',methods=['POST'])
+@app.route('/api/v1/pafu/',methods=['POST'])
 def prf():
      json_data=request.get_json()
      if not json_data:
