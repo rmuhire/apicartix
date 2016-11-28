bf360c128d15c4f19bbed503365ba39439d03565
hirwa dieume <dhirwa@exuus.com>
2016-11-28 17:01:07 +0200

    Naming APIs

--- app/api/get.py
+++ app/api/get.py
@@ -2,25 +2,27 @@ from app import *
 from app.model.models import *
 from app.model.schema import *
 from flask import request
-#import datetime
 from datetime import date
-#from datetime import datetime, timedelta
 from datetime import time
-#from datetime import timedelta
 from flask import jsonify
+from app.controller.cont import *
+from flask_mail import Mail, Message
+from random import randint
+
+mail=Mail(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return 'Error 404: Page not found, Please check ur route well.'
 
-@app.route('/getsavinggroup')
+@app.route('/api/v1/getsavinggroup')
 def get_svg():
     svgs=SavingGroup.query.all()
     result=sgs_schema.dump(svgs)
     return jsonify({'Saving groups':result.data})
 
 
-@app.route('/getsavinggroup/<name>')
+@app.route('/api/v1/getsavinggroup/<name>')
 def get_svgname(name):
     svg = SavingGroup.query.filter_by(name=name).first()
     if svg :
@@ -30,13 +32,13 @@ def get_svgname(name):
         return jsonify({'Message':'0'})
 
 
-@app.route('/getAmount')
+@app.route('/api/v1/getAmount')
 def get_am():
     ams = Amount.query.all()
     result = amounts_schema.dump(ams).data
     return jsonify({'Amounts':result})
 
-@app.route('/getAmount/<int:sgid>')
+@app.route('/api/v1/getAmount/<int:sgid>')
 def get_ams(sgid):
     ams = Amount.query.filter_by(sg_id=sgid).first()
     if ams:
@@ -45,45 +47,30 @@ def get_ams(sgid):
     else:
         return jsonify({'Message':'0'})
 
-@app.route('/getfundings')
+@app.route('/api/v1/getngos')
 def get_fu():
-    funs = Funding.query.all()
-    result = fundings_schema.dump(funs).data
-    return jsonify({'Funding NGOs':result})
+    funs = Ngo.query.all()
+    result = ngos_schema.dump(funs).data
+    return jsonify({'NGOs':result})
 
-@app.route('/getfunding/<name>')
+@app.route('/api/v1/getngo/<name>')
 def get_fun(name):
-    fun = Funding.query.filter_by(name=name)
-    if fun:
-        result = funding_schema.dump(fun)
-        return jsonify({'Funding NGO':result.data})
+    ng = Ngo.query.filter_by(name=name).first()
+    if ng:
+        result = ngo_schema.dump(ng)
+        return jsonify({'NGO':result.data})
 
     else:
         return jsonify({'Message':'0'})
 
-@app.route('/getpartners')
-def get_pa():
-    parts = Partner.query.all()
-    result = partners_schema.dump(parts).data
-    return jsonify({'Partner NGOs':result})
 
-@app.route('/getpartner/<name>')
-def get_pas(name):
-    fun = Funding.query.filter_by(name=name)
-    if fun:
-        result = funding_schema.dump(fun)
-        return jsonify({'Partner NGO':result.data})
-
-    else:
-        return jsonify({'Message':'0'})
-
-@app.route('/getFuPas')
+@app.route('/api/v1/getFuPas')
 def get_fupas():
     fupa = Sgs.query.all()
     result = sgfps_schema.dump(fupa)
     return jsonify({'Funding-Partner':result.data})
 
-@app.route('/getFuPa/<int:id>')
+@app.route('/api/v1/getFuPa/<int:id>')
 def getfup(id):
     fupa = Sgs.query.filter_by(id=id)
     if fupa:
@@ -94,35 +81,69 @@ def getfup(id):
 
 #===========================================LOG IN==================================
 
-@app.route('/funding/login/',methods=['POST'])
-def f_login():
+@app.route('/api/v1/login/',methods=['POST'])
+def login():
     json_data = request.get_json()
     if not json_data:
         return jsonify({'Message':'No input data provided'}), 400
-    data,errors=funding_schema.load(json_data)
+    data,errors = ngo_schema.load(json_data)
     if errors:
         return jsonify(errors), 422
     name,password = data['name'],data['password']
-    funding_ngo=Funding.query.filter_by(name=name,password=password).first()
-    if funding_ngo is None:
+    ngo = Ngo.query.filter_by(name=name,password=password).first()
+    if ngo is None:
         return jsonify({'Message':'0'})
     else:
-        res=funding_schema.dump(Funding.query.get(funding_ngo.id))
-        return jsonify({'Message':'1','Funding_NGO':res.data})
+        res = ngo_schema.dump(Ngo.query.get(ngo.id))
+        return jsonify({'Message':'1','NGO':res.data})
+
+#======================================== PASSWORD RECOVERY ==========================
 
 
-@app.route('/partner/login/',methods=['POST'])
-def p_login():
-    json_data = request.get_json()
-    if not json_data:
-        return jsonify({'Message':'No input data provided'}), 400
-    data,errors=partner_schema.load(json_data)
-    if errors:
-        return jsonify(errors), 422
-    name,password = data['name'],data['password']
-    partner_ngo=Partner.query.filter_by(name=name,password=password).first()
-    if partner_ngo is None:
+@app.route("/api/v1/recover/<email>")
+def recover(email):
+    df=Ngo.query.filter_by(email=email).first()
+    if df is None:
+        return jsonify({'Message':"0"})
+    else:
+        x=randint(100, 9999)
+        msg = Message('Hello', sender = 'getlunchex@gmail.com', recipients = [df.email])
+        msg.body = "your code is {}".format(x)
+        mail.send(msg)
+        dd=x
+
+        try:
+            sa=Cover.query.filter_by(ngo_id=df.id)
+            if sa is None:
+                db.session.delete(sa)
+                fj=Cover(ngo_id = df.id,code = dd)
+                db.session.add(fj)
+                db.session.commit()
+                return jsonify({"Message":"1"})
+            else:
+                fj=Cover(ngo_id = df.id,code = dd)
+                db.session.add(fj)
+                db.session.commit()
+                return jsonify({"Message":"1"})
+
+        except:
+            return jsonify({'Message':'Error'})
+
+
+@app.route("/api/v1/rec/<code>/<pas>")
+def rec(code,pas):
+    ll=Cover.query.filter_by(code = code ).order_by(Cover.ngo_id.desc()).first()
+    if ll is None:
         return jsonify({'Message':'0'})
     else:
-        res=partner_schema.dump(Partner.query.get(partner_ngo.id))
-        return jsonify({'Message':'1','Partner_NGO':res.data})
+        ld=Ngo.query.filter_by(id=ll.ngo_id).first()
+        if ld is None:
+            return jsonify({'Message':'0'})
+        else:
+            json_data=ngo_schema.dump(ld).data
+            pas = changePass(json_data,pas);
+
+            ld.password=pas
+            db.session.commit()
+
+            return jsonify({"Message":'1'})
