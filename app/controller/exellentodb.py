5dc2c001952149fb13b57cb0b0402a33f6e7dbcd
Remy Muhire <rmuhire@exuus.com>
2016-11-28 16:31:52 +0200

    move excel to excellentodb

--- app/controller/exellentodb.py
+++ app/controller/exellentodb.py
@@ -1,13 +1,119 @@
-from flask import jsonify
 from app.model.models import *
+from app.controller.exellento import Excellento
+from sqlalchemy.exc import IntegrityError
 
-from exellento import Excellento
+class Excellentodb:
+    def __init__(self, file):
+        self.file = file
+        self.json_data = Excellento(self.file).json()
+
+    def todb(self):
+        for data in self.json_data:
+            try:
+                saving = SavingGroup(
+                    name=data['saving_group_name'],
+                    year=data['sgs_year_of_creation'],
+                    member_female=data['sgs_members__female'],
+                    member_male=data['sgs_members__male'],
+                    sector_id=data['sector_code'],
+                    regDate=None
+                )
+                db.session.add(saving)
+                db.session.commit()
+            except IntegrityError:
+                db.session().rollback()
+                saving = SavingGroup.query.filter_by(name=data['saving_group_name']).first()
+
+            # Amount
+
+            saving_amount = data['saved_amount_as_of_december_2014']
+            if data['saved_amount_as_of_december_2014'] == 'N/A':
+                saving_amount = -1
+
+            borrowing_amount = data['outstanding_loans_as_of_december_2014']
+            if data['outstanding_loans_as_of_december_2014'] == 'N/A':
+                borrowing_amount = -1
+
+            amount = Amount(
+                saving= saving_amount,
+                borrowing=borrowing_amount,
+                year=2014,
+                sg_id=saving.id
+            )
+
+            db.session.add(amount)
+            db.session.commit()
+
+            # International NGO
+
+            try:
+                ngo = Ngo(
+                    name=data['funding_ngo'],
+                    email=None,
+                    telephone=None,
+                    website=None,
+                    category='funding_ngo',
+                    picture=None,
+                    address=None,
+                    cp_name=None,
+                    cp_email=None,
+                    cp_telephone=None,
+                    username=None,
+                    password=None
+                )
+
+                db.session.add(ngo)
+                db.session.commit()
+                intl_ngo_id = ngo.id
 
 
-json_data = Excellento('svg.xls').json()
+            except IntegrityError:
+                db.session().rollback()
+                ngo = Ngo.query.filter_by(name=data['funding_ngo']).first()
+                intl_ngo_id = ngo.id
 
-print json_data
 
+
+            # Local NGO
+
+
+            try:
+                ngo = Ngo(
+                    name=data['partner_ngo'],
+                    email=None,
+                    telephone=None,
+                    website=None,
+                    category='partner_ngo',
+                    picture=None,
+                    address=None,
+                    cp_name=None,
+                    cp_email=None,
+                    cp_telephone=None,
+                    username=None,
+                    password=None
+                )
+
+                db.session.add(ngo)
+                db.session.commit()
+
+                local_ngo_id = ngo.id
+
+            except IntegrityError:
+                db.session().rollback()
+                ngo = Ngo.query.filter_by(name=data['partner_ngo']).first()
+                local_ngo_id = ngo.id
+
+            # SGS
+
+            sgs = Sgs(
+                partner_id=local_ngo_id,
+                funding_id=intl_ngo_id
+            )
+
+            db.session.add(sgs)
+            db.session.commit()
+
+        return 1
 
 
 
