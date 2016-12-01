09acb1890f1f91b8ecfd8f4c0da2c83be2c8809f
Remy Muhire <rmuhire@exuus.com>
2016-12-01 17:54:47 +0200

    toexcel

--- app/api/post.py
+++ app/api/post.py
@@ -7,7 +7,7 @@ from app.controller.exellentodb import Excellento
 
 @app.route('/api/v1/exellento',methods=['POST'])
 def excellento():
-    data = Excellentodb('all.xlsx').toexcel()
+    data = Excellentodb('sg_datas.xlsx').toexcel()
     return jsonify({'data':data})
 
 
