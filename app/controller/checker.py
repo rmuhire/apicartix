557e033720d398350b2e812e6dce0fcc34340b28
Remy Muhire <rmuhire@exuus.com>
2016-12-02 16:27:46 +0200

    checking condition

--- app/controller/checker.py
+++ app/controller/checker.py
@@ -6,7 +6,7 @@ class Checker:
 
     def __init__(self, data):
         self.province = data[0]
-        self.district = data[1]
+        self.district = data[1].lower().title()
         self.sector = data[2]
         self.member = data[3]
         self.saved = data[4]
@@ -16,30 +16,53 @@ class Checker:
     def checker(self):
 
         # Check Province
-        try:
-            Province('East').province()
+
+        province = json.loads(Province(str(self.province)).province())
+        self.status['province'] = 0
+        if province['province']:
             self.status['province'] = 1
-        except KeyError:
-            self.status['province'] = 0
+
 
         # Check District
 
-        try:
-            District('Bugesera').district()
+        district = json.loads(District(str(self.district)).district())
+        self.status['district'] = 0
+
+        if district['district']:
             self.status['district'] = 1
-        except KeyError:
-            self.status['district'] = 0
 
         # check Sector
+        self.status['sector'] = 0
+        if self.status['district']:
+            data = json.loads(District(str(self.district)).sector())
+            sectors = data[str(self.district)]
+            items = sectors[0]['sector']
+            for item in items:
+                if item['name'].lower() == str(self.sector.lower()):
+                    self.status['sector'] = 1
 
-        '''if self.status['district']:
-            sectors = json.loads(District(self.district).sector())
-            self.status['sector'] = 0
-            for sector in sectors:
-                if sector['name'].lower() == self.sector.lower():
-                    self.status['sector'] = 1'''
+        self.status['member'] = 1
+        if int(self.member) < 15 | int(self.member > 30):
+            self.status['member'] = 0
+
+        try:
+            self.status['saved'] = 1
+            if int(self.saved) < int(15 * 100):
+                self.status['saved'] = 0
+        except ValueError:
+            self.status['saved'] = 1
+
+        try:
+            self.status['borrow'] = 1
+            if int(self.saved) <= 0 & int(self.borrow) != 0:
+                self.status['saved'] = 0
+                self.status['borrow'] = 0
+        except ValueError:
+            self.status['borrow'] = 1
 
         return self.status
+
+
 
 
 
