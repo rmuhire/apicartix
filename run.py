691ce6d3441b237ead1053216e4d11e7f08bf2dd
Remy Muhire <rmuhire@exuus.com>
2016-12-05 17:03:59 +0200

    remove Cross Origin

--- run.py
+++ run.py
@@ -1,14 +1,7 @@
 from app import *
-from app.api.post import *
 from app.api.get import *
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+from app.api.post import *
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000, debug=True)
+    app.run(host='0.0.0.0',port=8000, debug=True, threaded=True)
