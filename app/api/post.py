2d1b45b7d4ffb3fc9b4b779e4b6d382d1d3007a7
hirwa dieume <dhirwa@exuus.com>
2016-12-06 10:59:57 +0200

    User post

--- app/api/post.py
+++ app/api/post.py
@@ -2,18 +2,25 @@ from app import *
 from app.model.models import *
 from app.model.schema import *
 from flask import jsonify,request
-<<<<<<< HEAD
+from flask import jsonify,request
 import datetime
-from app.controller.excellentodb import Excellentodb
-from app.controller.excellentodb import Excellento
-=======
 from app.controller.exellentodb import Excellentodb
 from app.controller.exellentodb import Excellento
->>>>>>> adbe3c21f962ebc11d982a0db95ded910abbc530
+from app.controller.get_username import *
+from sqlalchemy.exc import IntegrityError
 
+@app.route('/api/v1/exellento',methods=['POST'])
+def excellento():
+    data = Excellentodb('faking_it.xlsx').toexcel()
+    return jsonify({'data':data})
+
+
+@app.route('/api/v1/visualize', methods=['POST'])
+def visualize():
+    data = Excellento('all.xlsx').json()
+    return jsonify({'data':data})
 #==================================================INDIVIDUAL POST==========================================================
 
-<<<<<<< HEAD
 @app.route('/api/v1/savinggroup/',methods=['POST'])
 def svg():
     json_data=request.get_json()
@@ -22,20 +29,8 @@ def svg():
     data,errors=sg_schema.load(json_data)
     if errors:
         return jsonify(errors), 422
-=======
-@app.route('/api/v1/exellento',methods=['POST'])
-def excellento():
-    data = Excellentodb('fake_data.xlsx').toexcel()
-    return jsonify({'data':data})
->>>>>>> adbe3c21f962ebc11d982a0db95ded910abbc530
 
 
-@app.route('/api/v1/visualize', methods=['POST'])
-def visualize():
-    data = Excellento('all.xlsx').json()
-    return jsonify({'data':data})
-
-<<<<<<< HEAD
 @app.route('/api/v1/amount/',methods=['POST'])
 def amount():
     json_data=request.get_json()
@@ -60,38 +55,80 @@ def amount():
     except:
         return jsonify({'Message':'0'})
 
-@app.route('/api/v1/ngo/',methods=['POST'])
-def fundg():
-    json_data=request.get_json()
+
+@app.route('/api/v1/user/', methods=['POST'])
+def add_user():
+    json_data = request.get_json()
+
     if not json_data:
-        return jsonify({'Message':'No data provided'})
-    data,errors=ngo_schema.load(json_data)
+        return jsonify({'message': 'No input data provided'}), 400
+
+    data, errors = user_schema.load(json_data)
+
     if errors:
         return jsonify(errors), 422
 
-    pw_hash = bcrypt.generate_password_hash(data['password'])
+    username = get_username(data['email'])
+    pwd_hash = bcrypt.generate_password_hash(data['password'])
 
-    #try:
-    ngo = Ngo(
-            name= data['name'],
-            email= data['email'],
-            username = data['username'],
-            category = data['category'],
-            website = "",
-            picture= "",
-            telephone = "",
-            address = data['address'],
-            cp_name = data['cp_name'],
-            cp_email = data['cp_email'],
-            cp_telephone = data['cp_telephone'],
-            password = pw_hash
-            )
-    db.session.add(ngo)
-    db.session.commit()
-    result = ngo_schema.dump(Ngo.query.get(ngo.id))
-    return jsonify({'NGO':result.data})
-    #except:
-    #    return jsonify({'Message':'0'})
+    try:
+        user = User(
+            names=data['names'],
+            username=username,
+            email=data['email'],
+            phone=None,
+            user_role=None,
+            regDate=None,
+            password=pwd_hash,
+            gender=None,
+            ngo_id=data['ngo_id']
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        last_user = user_schema.dump(User.query.get(user.id)).data
+        return jsonify({'auth':1, 'user':last_user})
+
+    except IntegrityError:
+        return jsonify({'auth': 0, 'user': 'Already added.'})
+
+
+@app.route('/api/v1/ngo/', methods=['POST'])
+def add_ngo():
+    json_data = request.get_json()
+
+    if not json_data:
+        return jsonify({'message':'No input data provided'}), 400
+
+    data, errors = ngo_schema.load(json_data)
+
+    if errors:
+        return jsonify(errors), 422
+
+    try:
+        ngo = Ngo(
+            name=data['name'].upper(),
+            email=data['email'],
+            telephone=None,
+            website=None,
+            category=data['category'],
+            picture=None,
+            address=None
+        )
+
+        db.session.add(ngo)
+        db.session.commit()
+
+        last_ngo = ngo_schema.dump(Ngo.query.get(ngo.id)).data
+        return jsonify({'auth':1, 'ngo':last_ngo})
+
+    except IntegrityError:
+        db.session().rollback()
+        ngo = Ngo.query.filter_by(name=data['name'].upper()).first()
+        ngo_id = ngo.id
+        return jsonify({'auth': 0, 'ngo': ngo_id})
+
 
 @app.route('/api/v1/pafu/',methods=['POST'])
 def prf():
@@ -109,19 +146,6 @@ def prf():
      db.session.commit()
      result=sgfp_schema.dump(Sgs.query.get(sgss.id))
 
-
-@app.route('/api/v1/excellento',methods=['POST'])
-def excellento():
-    data = Excellentodb('faking_it.xlsx').todb()
-    return jsonify({'data':data})
-
-
-@app.route('/api/v1/visualize', methods=['POST'])
-def visualize():
-    data = Excellento('sg_datas.xlsx').json()
-    return jsonify({'data':data})
-
-
 #===========================================LOG IN==================================
 
 @app.route('/api/v1/login/',methods=['POST'])
@@ -129,31 +153,21 @@ def login():
     json_data = request.get_json()
     if not json_data:
         return jsonify({'Message':'No input data provided'}), 400
-    data,errors = ngo_schema.load(json_data)
+    data,errors = user_schema.load(json_data)
 
     if errors:
         return jsonify(errors), 422
 
-    name,password = data['name'],data['password']
+    username,password = data['username'],data['password']
 
-    ngo = Ngo.query.filter(Ngo.name==name).first()
+    user = User.query.filter((User.username==username) | (User.email==username)).first()
 
     try:
         pw_hash = bcrypt.check_password_hash(ngo.password, password)
         if pw_hash:
-            result = ngo_schema.dump(Ngo.query.get(ngo.id))
-            return jsonify({'auth': 1, 'ngo': result.data})
+            result = user_schema.dump(User.query.get(user.id))
+            return jsonify({'auth': 1, 'user': result.data})
         else:
             return jsonify({'auth': 0})
     except AttributeError:
         return jsonify({'auth':2})
-
-
-    #ngo = Ngo.query.filter_by(name=name,password=password).first()
-    #if ngo is None:
-    #    return jsonify({'Message':'0'})
-    #else:
-    #    res = ngo_schema.dump(Ngo.query.get(ngo.id))
-    #    return jsonify({'Message':'1','NGO':res.data})
-=======
->>>>>>> adbe3c21f962ebc11d982a0db95ded910abbc530
