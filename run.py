32835ecf9f3e335ba124dceeb7fea893f843f4cf
Remy Muhire <rmuhire@exuus.com>
2016-12-10 10:28:19 +0200

    run.py

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from app.api.get import *
 from app.api.post import *
 
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8000, debug=True, threaded=True)
+    app.run(host='0.0.0.0',port=5000, debug=True, threaded=True)
