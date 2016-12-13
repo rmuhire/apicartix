f55308c27f1d5b9d34279849b1bc329dfecf56c5
Remy Muhire <rmuhire@exuus.com>
2016-12-13 17:18:55 +0200

    adding session

--- app/api/post.py
+++ app/api/post.py
@@ -1,6 +1,6 @@
 from app.model.models import *
 from app.model.schema import *
-from flask import jsonify,request
+from flask import jsonify,request, session
 from app.controller.exellentodb import Excellentodb
 from app.controller.exellentodb import Excellento
 from sqlalchemy.exc import IntegrityError
@@ -28,7 +28,7 @@ def visualize():
     return jsonify({'data':data})
 
 
-@app.route('/api/v1/user', methods=['POST'])
+@app.route('/api/v1/user/', methods=['POST'])
 def add_user():
     json_data = request.get_json()
 
@@ -60,13 +60,13 @@ def add_user():
         db.session.commit()
 
         last_user = user_schema.dump(User.query.get(user.id)).data
-        return jsonify({'auth':1, 'user':last_user})
+        return jsonify({'result':last_user})
 
     except IntegrityError:
-        return jsonify({'auth': 0, 'message': 'Already added.'})
+        return jsonify({'result': '0'})
 
 
-@app.route('/api/v1/ngo', methods=['POST'])
+@app.route('/api/v1/ngo/', methods=['POST'])
 def add_ngo():
     json_data = request.get_json()
 
@@ -93,13 +93,13 @@ def add_ngo():
         db.session.commit()
 
         last_ngo = ngo_schema.dump(Ngo.query.get(ngo.id)).data
-        return jsonify({'auth':1, 'ngo':last_ngo})
+        return jsonify({'result': True})
 
     except IntegrityError:
         db.session().rollback()
         ngo = Ngo.query.filter_by(name=data['name'].upper()).first()
         ngo_id = ngo.id
-        return jsonify({'auth': 0, 'ngo': ngo_id})
+        return jsonify({'result': False})
 
 
 @app.route("/api/v1/login/", methods=['POST'])
@@ -118,12 +118,16 @@ def login():
     try:
         pw_hash = bcrypt.check_password_hash(user.password, password)
         if pw_hash:
-            result = user_schema.dump(User.query.get(user.id))
-            return jsonify({'auth': 1, 'user': result.data})
+            session['logged_in'] = True
+            session['user_id'] = user.id
+            status = True
+            return jsonify({'result': status})
         else:
-            return jsonify({'auth': 0})
+            status = False
+            return jsonify({'result': status})
     except AttributeError:
-        return jsonify({'auth':2})
+        status = False
+        return jsonify({'result': status})
 
 
 
