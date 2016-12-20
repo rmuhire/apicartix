6ab040c6d11f05938b87a63b6de6958c0fb74bf9
rmuhire <rmuhire@exuus.com>
2016-12-20 10:10:23 +0200

    __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 app.config.from_object(BaseConfig)
 
 db = SQLAlchemy(app)
