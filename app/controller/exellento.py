e9b875c29cae243fd4e591931e75574e53807dc2
rmuhire <rmuhire@exuus.com>
2017-01-09 17:23:29 +0200

    exellento

--- app/controller/exellento.py
+++ app/controller/exellento.py
@@ -1,11 +1,12 @@
 from xlrd import open_workbook
 import json
+from saving_year import saving_year
 
 
 class Excellento:
 
     def __init__(self, filepath):
-        self.filepath =filepath
+        self.filepath = filepath
         self.wb = open_workbook(self.filepath)
 
     def json(self):
@@ -16,8 +17,19 @@ class Excellento:
             for row in range(0, 1):
                 header_sheet = []
                 for col in range(columns):
-                    header_cell = sheet.cell(row, col).value.replace(" ","_").replace("-","")
-                    header_sheet.append(header_cell.lower().replace("_(in_rwf)",""))
+                    if col == 11:
+                        data = sheet.cell(row, col).value
+                        val = saving_year(data)
+                        header_sheet.append(val[1])
+                    elif col == 12:
+                        data = sheet.cell(row, col).value
+                        val = saving_year(data)
+                        header_sheet.append(val[2])
+                    else:
+                        header_cell = sheet.cell(row, col).value.replace(" ","_").replace("-","")
+                        header_sheet.append(header_cell.lower())
+
+            year = val[0]
 
             items = []
 
@@ -25,7 +37,6 @@ class Excellento:
                 data_json = {}
                 for col in range(columns):
                     value = sheet.cell(row, col).value
-                    
                     try:
                         value = str(int(value))
 
@@ -33,8 +44,9 @@ class Excellento:
                         pass
                     finally:
                         data_json[header_sheet[col]] = value
-
+                data_json["year_amount"] = year
                 items.append(data_json)
+
         return items
 
 
