bece8dad2e8715abac7d8a72f469f9db83296751
rmuhire <rmuhire@exuus.com>
2017-01-09 17:21:11 +0200

    saving_year controller

---
+++ app/controller/saving_year.py
@@ -0,0 +1,22 @@
+import datetime
+
+
+def saving_year(data):
+    data = data.split(" ")
+    year = generate_year()
+    for y in year:
+        for d in data:
+            if str(y) == d:
+                return [y,"saved_amount","outstanding_loans"]
+
+
+def generate_year():
+    now = datetime.datetime.now()
+    current_year = now.year
+    years = []
+    for i in range(2014, current_year+1):
+        years.append(i)
+    return years
+
+
+
