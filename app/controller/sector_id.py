c9603ccf6ac5a48b3f27426672e543a3895bb003
rmuhire <rmuhire@exuus.com>
2017-01-12 16:58:09 +0200

    bug fixed

--- app/controller/sector_id.py
+++ app/controller/sector_id.py
@@ -1,14 +1,11 @@
 from kenessa import District
-import json
 
 
 def sector_id(sector, district):
     district = district.lower().title()
-    data = json.loads(District(str(district)).sector())
-    sectors = data[str(district)]
-    items = sectors[0]['sector']
+    data = District(str(district)).sector()
 
-    for item in items:
+    for item in data:
         if item['name'].lower() == str(sector.lower()):
             return item['id']
 
