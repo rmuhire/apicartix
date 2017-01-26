505c23c785e9513648f8369956575ddbf0a4d09d
rmuhire <rmuhire@exuus.com>
2017-01-26 10:31:19 +0200

    models

--- app/model/models.py
+++ app/model/models.py
@@ -139,12 +139,14 @@ class Files(db.Model):
     saved = db.Column(db.String(175))
     filename = db.Column(db.String(150))
     regDate = db.Column(db.DateTime)
+    status = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, original, saved, filename, user_id, regDate=None):
+    def __init__(self, original, saved, filename, status, user_id, regDate=None):
         self.original = original
         self.saved = saved
         self.filename = filename
+        self.status = status
         self.user_id = user_id
         if regDate is None:
             regDate = datetime.utcnow()
