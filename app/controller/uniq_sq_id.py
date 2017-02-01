e8aea4d763723e130c3dde02c04c084b6ceaa2e9
rmuhire <rmuhire@exuus.com>
2017-02-01 12:24:10 +0200

    uniq_sq_id

--- app/controller/uniq_sq_id.py
+++ app/controller/uniq_sq_id.py
@@ -3,3 +3,8 @@ def uniq_sg_id(intl_ngo_id, local_ngo_id, s_id, sg_name):
     id = ''.join([intl_ngo_id,local_ngo_id, s_id, sg_name])
 
     return id
+
+
+def uniq_id_amount(year_amount, sg_id):
+    id = ''.join([year_amount, sg_id])
+    return id
