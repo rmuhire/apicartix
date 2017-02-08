4ac9792f082fc9cce20010de1cb3aed5a301565d
rmuhire <rmuhire@exuus.com>
2017-02-08 12:38:34 +0200

    list partner filter

--- app/controller/list_partner.py
+++ app/controller/list_partner.py
@@ -1,4 +1,9 @@
 def litPartnerNgo(items):
     uniqueList = {item['partner_id']: item for item in items}
-    
+    partner_id = []
+    for id in uniqueList:
+        partner_id.append(id)
+
+    return partner_id
+
 
