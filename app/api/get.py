f994222cfa344679ebedc1bbc59614341cc84680
rmuhire <rmuhire@exuus.com>
2017-03-15 10:02:45 +0200

    map analytics queries

--- app/api/get.py
+++ app/api/get.py
@@ -7,7 +7,7 @@ from app.template.email import Email
 import json
 from app.controller.saving_year import generate_year
 from app.controller.list_partner import litPartnerNgo
-from sqlalchemy import and_
+from sqlalchemy import func, text
 
 
 
@@ -209,9 +209,68 @@ def get_user_file(id):
 
 @app.route('/api/v1/saving')
 def saving_group():
-    sg = SavingGroup.query.all()
-    if sg:
-        result = sgs_schema.dump(sg).data
-        return jsonify(result)
+    sg = db.session.query(db.func.count(SavingGroup.name),
+                          db.func.sum(SavingGroup.member_female),
+                          db.func.sum(SavingGroup.member_male),
+                          db.func.sum(SavingGroup.borrowing),
+                          db.func.sum(SavingGroup.saving),
+                          SavingGroup.sector_id).\
+        join(Sector, SavingGroup.sector_id == Sector.id).\
+        filter(SavingGroup.sector_id == Sector.id).\
+        group_by(SavingGroup.sector_id).all()
+    return jsonify([i for i in sg])
 
+
+
+@app.route('/api/v1/sqlsaving')
+def sql_saving():
+    sql_province = text('select count(saving_group.name)'
+               ', sum(saving_group.member_female) '
+               ', sum(saving_group.member_male)'
+               ', sum(saving_group.borrowing)'
+               ', sum(saving_group.saving)'
+               ', province.name '
+               'from saving_group, sector, district, province '
+               'where sector.id = saving_group.sector_id AND '
+               'district.id = sector.district_id AND '
+               'province.id = district.province_id '
+               'group by province.id')
+    result = db.engine.execute(sql_province)
+    province = []
+    for row in result:
+        data = [row[0], row[1], row[2], row[3], row[4], row[5]]
+        province.append(data)
+
+    sql_district = text('select count(saving_group.name)'
+                       ', sum(saving_group.member_female) '
+                       ', sum(saving_group.member_male)'
+                       ', sum(saving_group.borrowing)'
+                       ', sum(saving_group.saving)'
+                       ', district.name '
+                       'from saving_group, sector, district '
+                       'where sector.id = saving_group.sector_id AND '
+                       'district.id = sector.district_id '
+                       'group by district.id')
+    result = db.engine.execute(sql_district)
+    district = []
+    for row in result:
+        data = [row[0], row[1], row[2], row[3], row[4], row[5]]
+        district.append(data)
+
+    sql_district = text('select count(saving_group.name)'
+                        ', sum(saving_group.member_female) '
+                        ', sum(saving_group.member_male)'
+                        ', sum(saving_group.borrowing)'
+                        ', sum(saving_group.saving)'
+                        ', sector.name '
+                        'from saving_group, sector '
+                        'where sector.id = saving_group.sector_id '
+                        'group by sector.id')
+    result = db.engine.execute(sql_district)
+    sector = []
+    for row in result:
+        data = [row[0], row[1], row[2], row[3], row[4], row[5]]
+        sector.append(data)
+
+    return jsonify({'province':province, 'district':district, 'sector':sector})
 
