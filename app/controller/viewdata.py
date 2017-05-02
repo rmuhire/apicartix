3a522ad886c8530e388934f04f54e716558a4681
rmuhire <rmuhire@exuus.com>
2017-05-02 19:24:07 +0200

    viewData

---
+++ app/controller/viewdata.py
@@ -0,0 +1,102 @@
+from app.model.models import *
+from sqlalchemy import text
+
+
+class ViewData:
+    def __init__(self, province, district, sector, ngo):
+        self.provinces = province.split(",")
+        self.districts = district.split(",")
+        self.sectors = sector.split(",")
+        self.ngos = ngo.split(",")
+
+    def viewData(self):
+        query = "select count(saving_group.id)," \
+                " sum(saving_group.member_female)," \
+                " sum(saving_group.member_male)," \
+                " sum(saving_group.saving)," \
+                " sum(saving_group.borrowing)" \
+                " from saving_group," \
+                " province," \
+                " district," \
+                " sector," \
+                " ngo" \
+                " where saving_group.sector_id =sector.id" \
+                " and sector.district_id = district.id" \
+                " and district.province_id = province.id" \
+                " and saving_group.partner_id = ngo.id"
+
+        if self.provinces != ['null']:
+            mini_query = miniQueryProvince(self.provinces)
+            query += " and "+ mini_query
+
+        if self.districts != ['null']:
+            mini_query = miniQueryDistrict(self.districts)
+            query += " and " + mini_query
+
+        if self.sectors != ['null']:
+            mini_query = miniQuerySector(self.sectors)
+            query += " and " + mini_query
+
+        if self.ngos != ['null']:
+            mini_query = miniQueryNgo(self.ngos)
+            query += " and " + mini_query
+
+        sql_query = text(query)
+        result = db.engine.execute(sql_query)
+
+        for row in result:
+            data = [row[0], row[1], row[2], row[3], row[4]]
+
+        return [data, query]
+
+
+def miniQueryProvince(provinces):
+    mini_query = "province.id IN ("
+    for i, province in enumerate(provinces):
+        if i == len(provinces) - 1:
+            mini_query += province
+        else:
+            mini_query += province + ","
+
+    mini_query += ")"
+
+    return mini_query
+
+
+def miniQueryDistrict(districts):
+    mini_query = "district.id IN ("
+    for i, district in enumerate(districts):
+        if i == len(districts) - 1:
+            mini_query += district
+        else:
+            mini_query += district + ","
+
+    mini_query += ")"
+
+    return mini_query
+
+
+def miniQuerySector(sectors):
+    mini_query = "sector.id IN ("
+    for i, sector in enumerate(sectors):
+        if i == len(sectors) - 1:
+            mini_query += sector
+        else:
+            mini_query += sector + ","
+
+    mini_query += ")"
+
+    return mini_query
+
+
+def miniQueryNgo(ngos):
+    mini_query = "ngo.id IN ("
+    for i, ngo in enumerate(ngos):
+        if i == len(ngos) - 1:
+            mini_query += ngo
+        else:
+            mini_query += ngo + ","
+
+    mini_query += ")"
+
+    return mini_query
