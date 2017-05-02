9b008d51e06d30f7beacb6e841a411b96a98a58e
rmuhire <rmuhire@exuus.com>
2017-05-02 20:20:19 +0200

    view data

--- app/controller/viewdata.py
+++ app/controller/viewdata.py
@@ -47,7 +47,152 @@ class ViewData:
         for row in result:
             data = [row[0], row[1], row[2], row[3], row[4]]
 
-        return [data, query]
+        return data
+
+    def viewDataGraduated(self):
+        query = "select count(sg_status)" \
+                " from saving_group," \
+                " province," \
+                " district," \
+                " sector," \
+                " ngo" \
+                " where saving_group.sector_id =sector.id" \
+                " and sector.district_id = district.id" \
+                " and district.province_id = province.id" \
+                " and saving_group.partner_id = ngo.id" \
+                " and sg_status = :val"
+
+        if self.provinces != ['null']:
+            mini_query = miniQueryProvince(self.provinces)
+            query += " and "+ mini_query
+
+        if self.districts != ['null']:
+            mini_query = miniQueryDistrict(self.districts)
+            query += " and " + mini_query
+
+        if self.sectors != ['null']:
+            mini_query = miniQuerySector(self.sectors)
+            query += " and " + mini_query
+
+        if self.ngos != ['null']:
+            mini_query = miniQueryNgo(self.ngos)
+            query += " and " + mini_query
+
+        sql_query = text(query)
+        result = db.engine.execute(sql_query, val='Graduated')
+        for row in result:
+            data = row[0]
+
+        return data
+
+    def viewDataSupervised(self):
+        query = "select count(sg_status)" \
+                " from saving_group," \
+                " province," \
+                " district," \
+                " sector," \
+                " ngo" \
+                " where saving_group.sector_id =sector.id" \
+                " and sector.district_id = district.id" \
+                " and district.province_id = province.id" \
+                " and saving_group.partner_id = ngo.id" \
+                " and sg_status = :val"
+
+        if self.provinces != ['null']:
+            mini_query = miniQueryProvince(self.provinces)
+            query += " and " + mini_query
+
+        if self.districts != ['null']:
+            mini_query = miniQueryDistrict(self.districts)
+            query += " and " + mini_query
+
+        if self.sectors != ['null']:
+            mini_query = miniQuerySector(self.sectors)
+            query += " and " + mini_query
+
+        if self.ngos != ['null']:
+            mini_query = miniQueryNgo(self.ngos)
+            query += " and " + mini_query
+
+        sql_query = text(query)
+        result = db.engine.execute(sql_query, val='Supervised')
+        for row in result:
+            data = row[0]
+
+        return data
+
+    def viewDataYearOfCreation(self):
+        query = "select distinct(year_of_creation)" \
+                " from saving_group," \
+                " province," \
+                " district," \
+                " sector," \
+                " ngo" \
+                " where saving_group.sector_id =sector.id" \
+                " and sector.district_id = district.id" \
+                " and district.province_id = province.id" \
+                " and saving_group.partner_id = ngo.id"
+
+        if self.provinces != ['null']:
+            mini_query = miniQueryProvince(self.provinces)
+            query += " and " + mini_query
+
+        if self.districts != ['null']:
+            mini_query = miniQueryDistrict(self.districts)
+            query += " and " + mini_query
+
+        if self.sectors != ['null']:
+            mini_query = miniQuerySector(self.sectors)
+            query += " and " + mini_query
+
+        if self.ngos != ['null']:
+            mini_query = miniQueryNgo(self.ngos)
+            query += " and " + mini_query
+
+        sql_query = text(query)
+        result = db.engine.execute(sql_query)
+        data = list()
+        for row in result:
+            data.append(row[0])
+
+        return data
+
+    def viewDataFundingNgo(self):
+        query = "select distinct(saving_group.funding_id)" \
+                " from saving_group," \
+                " province," \
+                " district," \
+                " sector," \
+                " ngo" \
+                " where saving_group.sector_id =sector.id" \
+                " and sector.district_id = district.id" \
+                " and district.province_id = province.id" \
+                " and saving_group.partner_id = ngo.id"
+
+        if self.provinces != ['null']:
+            mini_query = miniQueryProvince(self.provinces)
+            query += " and " + mini_query
+
+        if self.districts != ['null']:
+            mini_query = miniQueryDistrict(self.districts)
+            query += " and " + mini_query
+
+        if self.sectors != ['null']:
+            mini_query = miniQuerySector(self.sectors)
+            query += " and " + mini_query
+
+        if self.ngos != ['null']:
+            mini_query = miniQueryNgo(self.ngos)
+            query += " and " + mini_query
+
+        sql_query = text(query)
+        result = db.engine.execute(sql_query)
+        data = list()
+        for row in result:
+            data.append(ngoName(row[0]))
+
+        return data
+
 
 
 def miniQueryProvince(provinces):
@@ -100,3 +245,10 @@ def miniQueryNgo(ngos):
     mini_query += ")"
 
     return mini_query
+
+
+def ngoName(id):
+    sql = text('select name from ngo where id = :id')
+    result = db.engine.execute(sql, id=id)
+    for row in result:
+        return row[0]
