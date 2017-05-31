1a6211d0873e6808471627a3456de26ed0a45839
remymuhire <jeanremy1806@gmail.com>
2017-05-31 14:35:26 +0200

    put py

--- app/api/put.py
+++ app/api/put.py
@@ -2,6 +2,7 @@ from app.model.models import *
 from flask_bcrypt import Bcrypt
 from flask import jsonify,request
 from app.template.email import Email
+from sqlalchemy.exc import IntegrityError
 
 bcrypt = Bcrypt(app)
 
@@ -26,5 +27,59 @@ def change_password():
     return jsonify({'result':status})
 
 
+@app.route('/api/v1/users/<int:id>', methods=['PUT'])
+def edit_users(id):
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'result':False})
 
+    names = json_data['names']
+    email = json_data['email']
+
+    try:
+        user = User.query.get(id)
+        user.email = email
+        user.names = names
+        db.session.commit()
+        return jsonify(True)
+    except IntegrityError:
+        return jsonify(False)
+
+
+@app.route('/api/v1/users/check_password/<int:id>', methods=['PUT'])
+def check_password(id):
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'result': False})
+
+    password = json_data['password']
+    try:
+        user = User.query.get(id)
+        pw_hash = bcrypt.check_password_hash(user.password, password)
+        if pw_hash:
+            return jsonify(True)
+        else:
+            return jsonify(False)
+
+    except IntegrityError:
+        return jsonify(False)
+
+
+@app.route('/api/v1/change/password/<int:id>', methods=["PUT"])
+def change_password_id(id):
+    json_data = request.get_json()
+    if not json_data:
+        return jsonify({'result':False})
+
+    pw_hash = bcrypt.generate_password_hash(json_data['password'])
+
+    user = User.query.get(id)
+
+    user.password = pw_hash
+    user.update_key = None
+    db.session.commit()
+
+    status = Email(user.names, user.username, user.email).resetsuccess()
+
+    return jsonify(status)
 
